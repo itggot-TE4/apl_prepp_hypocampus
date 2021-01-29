e016d53bd83d286f92035ec84e28e0b9bcffe1aa
zander <alexander.kjellberg2@elev.ga.ntig.se>
2021-01-29 12:07:23 +0100

    start on circle

---
+++ apl_prepp/src/components/CircleButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="rounded-full h-24 w-24 flex items-center justify-center bg-red-500">
+    </div>
+    <h3>{{ universitys.chalmers }}</h3>
+  </div>
+  
+
+</template>
+
+<script>
+export default {
+  name: 'CircleButton',
+ data () {
+    return {
+      universitys: { 
+        chalmers: 'Chalmers',
+        gbgUnity: 'Göteborgs Universitet',
+       }
+    }
+ }
+  // props: ['icon', 'schoolname'],
+
+}
+</script>
