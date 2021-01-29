33c740b65860a5d30448921ca3cf9c6dd3dd3191
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2021-01-29 12:44:15 +0100

    ported to nuxt and added a simple navbar

---
+++ apl_prepp_nuxt/pages/Dashboard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Navbar />
+    <h1>This is DASHBOARD</h1>
+    <CircleButton />
+    <div id="chart_div">
+      <GChart
+        type="LineChart"
+        :data="chartData"
+        :options="chartOptions"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+import CircleButton from '../components/CircleButton.vue'
+import Navbar from '../components/Navbar.vue'
+
+export default {
+  name: 'Dashboard',
+
+  components: {
+    GChart,
+    CircleButton,
+    Navbar
+  },
+
+  data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ['Gün', 'Harcama', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+      ],
+      chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017'
+        }
+      }
+    }
+  }
+}
+</script>
